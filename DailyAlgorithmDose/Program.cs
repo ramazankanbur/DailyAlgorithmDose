8865ea8b826c5af96c55efac830af42b633101a6
ramazan kanbur <ramazank@koczer.com>
2022-04-30 21:39:41 +0300

    frog jump algorithm added

---
+++ DailyAlgorithmDose/Program.cs
@@ -0,0 +1,6 @@
+﻿using DailyAlgorithmDose.CodilityPlayground;
+
+Console.WriteLine($"FrogJmp ornegi -> {FrogJmp.Solution1(10, 500, 33)}");
+Console.WriteLine($"FrogJmp ornegi 2 -> {FrogJmp.Solution1(10, 500, 33)}");
+
+
