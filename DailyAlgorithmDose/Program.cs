c68ecf4a2914100258531613444164e9e6d64882
ramazan kanbur <ramazank@koczer.com>
2022-05-01 11:27:32 +0300

    permmissingelem implemented

--- DailyAlgorithmDose/Program.cs
+++ DailyAlgorithmDose/Program.cs
@@ -3,4 +3,4 @@
 Console.WriteLine($"FrogJmp ornegi -> {FrogJmp.Solution1(10, 500, 33)}");
 Console.WriteLine($"FrogJmp ornegi 2 -> {FrogJmp.Solution1(10, 500, 33)}");
 
-
+Console.WriteLine($"PermMissingElem ornegi -> {PermMissingElem.Solution(new int[] { 1, 2 })}");
