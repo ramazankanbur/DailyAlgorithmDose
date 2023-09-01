abf1c532a8500d2dcdec9fc28af114822298c623
ramazan kanbur <ramazan.kanbur@siemens.com>
2023-09-01 16:42:20 +0300

    valid parantheses sample added

---
+++ DailyAlgorithmDose/Others/ParenthesesSamples.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyAlgorithmDose.Others
+{
+    public class ParenthesesSamples
+    {
+        static List<string> paranthesis = new List<string>() { "(", ")" };
+
+        public static string GenerateValidParantheses(int length)
+        {
+            string result = string.Empty;
+
+            if (length % 2 != 0)
+            {
+                return "not valid input.";
+            }
+            
+            var iterationCountForOpen = length / 2;
+             
+            bool isCloseParanthesisValid = true;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (i == 0)
+                {
+                    result += paranthesis[i];
+                    iterationCountForOpen -= 1;
+                    continue;
+                }
+
+                Random random = new Random();
+                var randomParamthesis = random.Next(0, 2);
+                
+                if (paranthesis[randomParamthesis] == "(" && iterationCountForOpen > 0 || !isCloseParanthesisValid)
+                {
+                    result += "(";
+                    iterationCountForOpen -= 1;
+                    isCloseParanthesisValid = true;
+                }
+                else if (paranthesis[randomParamthesis] == ")" && isCloseParanthesisValid || iterationCountForOpen == 0)
+                {
+                    result += ")";
+                    if (result.Count(x => x == '(') <= result.Count(x => x == ')'))
+                    {
+                        isCloseParanthesisValid = false;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
--- DailyAlgorithmDose/Program.cs
+++ DailyAlgorithmDose/Program.cs
@@ -1,6 +1,9 @@
 ﻿using DailyAlgorithmDose.CodilityPlayground;
+using DailyAlgorithmDose.Others;
 
 Console.WriteLine($"FrogJmp ornegi -> {FrogJmp.Solution1(10, 500, 33)}");
 Console.WriteLine($"FrogJmp ornegi 2 -> {FrogJmp.Solution1(10, 500, 33)}");
 
 Console.WriteLine($"PermMissingElem ornegi -> {PermMissingElem.Solution(new int[] { 1, 2 })}");
+
+Console.WriteLine($"GenerateValidPatenheses ornegi -> {ParenthesesSamples.GenerateValidParantheses(8)}");
